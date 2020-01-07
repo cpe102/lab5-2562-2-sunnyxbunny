39c89f5cb8f08f40f04e869dea3dc7274b9fcc4c
xbunny <pitchayarat.p@gmail.com>
2020-01-07 15:49:45 +0700

    07012020

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -7,17 +7,29 @@ int main(){
 	cin >> N;
 	
 	string name[N];
-	int age[N];
-	cout << "Name of student = ";
-	cout << "Age of student = ";
+	int age[N], i=0;
 	
+	while(i<N){
+		cout << "Name of student "<<i+1<<" = ";
+		cin >> name[i];
+		cout << "Age of student "<<i+1<<" = ";
+		cin >> age[i];
+		i++;
+	}
+
 	int key;
 	cout << "--------------------------------------\n";
 	cout << "Enter an age to search: ";
 	cin >> key;
 	cout << "--------------------------------------\n";
 	
-
+	i=0;
+	while(i<N){
+		if(age[i]==key){
+		cout << name[i] <<"\n";
+		}
+		i++;	
+	}
 	
 	cout << "--------------------------------------\n";
 	
